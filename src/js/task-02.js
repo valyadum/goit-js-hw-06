9450d040b698d2a5f97ec98a850e2fabe06ad5ac
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2023-08-12 11:16:05 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients');
+const ingredientArr =  ingredients.map(ingredient => {
+  const itemsEl = document.createElement('li');
+  itemsEl.textContent = `${ingredient}`;
+  itemsEl.classList.add('item');
+  console.log(itemsEl);
+  return itemsEl;
+  
+  
+})
+list.append(...ingredientArr);
