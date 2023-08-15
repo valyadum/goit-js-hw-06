08efd524ba6c6f18c2a6b91e0da4f7d781a09754
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2023-08-15 15:21:54 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,13 @@
-
+let form = document.querySelector('.login-form');
+form.addEventListener('submit', onSubmitClick);
+function onSubmitClick(event){
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === '' || password.value === '') {
+        alert('Всі поля повинні бути заповнені');
+    }
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
