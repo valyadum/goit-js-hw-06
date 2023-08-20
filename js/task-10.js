5a5e602168d31e198ee664642fdab0b3a79bb498
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2023-08-20 11:04:09 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -8,28 +8,43 @@ const inputNumber = document.querySelector('input');
 const btnCreate = document.querySelector('button[data-create]');
 const btnDestroy = document.querySelector('button[data-destroy]');
 const boxes = document.querySelector('#boxes');
+const step = Number.parseInt(inputNumber.step);
+const minNumber = Number.parseInt(inputNumber.min);
+const maxNumber = Number.parseInt(inputNumber.max);
+let amount = 0;
 
-let amount = inputNumber.addEventListener('input', (event) => {
-   return   event.currentTarget.value;
-     
-  });
-console.log(amount);
 
 function createBoxes(amount) {
+  const colorBoxArr = [];
   let width = 30;
   let height = 30;
-  for (let index = 1; index <= amount; index++) {
-    const createDivElement = document.createElement('div');
-    createDivElement.style.width = width + "px";
-    createDivElement.style.height = height + "px";
-    createDivElement.style.backgroundColor = getRandomHexColor();
-    width += 10;
-    height += 10;
-
-    console.log(createDivElement);
-    boxes.append(createDivElement);
+  amount = inputNumber.value;
+  if (amount >= minNumber && amount <= maxNumber) {
+    for (let index = 1; index <= amount; index += step) {
+      const createDivElement = document.createElement('div');
+      createDivElement.classList.add("colorBox");
+      createDivElement.style.width = width + "px";
+      createDivElement.style.height = height + "px";
+      createDivElement.style.backgroundColor = getRandomHexColor();
+      width += 10;
+      height += 10;
+      colorBoxArr.push(createDivElement);
+      
+      //boxes.append(createDivElement);
+    }
+    console.log(colorBoxArr);
+    boxes.append(...colorBoxArr);
+    return;
   }
-
+  alert('Введено значення яке не відповідає вимогам!');
 }
 
+
+
+const divBox = document.querySelectorAll('.colorBox');
+function destroyBoxes() {  //нажаль не знаю як вірно очистити
+  inputNumber.textContent = ('');
+  divBox.remove();
+}
 btnCreate.addEventListener('click', createBoxes);
+btnCreate.addEventListener('click', destroyBoxes);
