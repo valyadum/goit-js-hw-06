dbd9c27ea8a2b761793ca4b4067c64993dd2e80c
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2023-08-22 11:26:16 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,13 +3,14 @@
     .toString(16)
     .padStart(6, 0)}`;
 }
-let randomColor = getRandomHexColor();
+
 let btn = document.querySelector('.change-color');
 let spanColor = document.querySelector('.color');
 let body = document.querySelector('body');
 
 btn.addEventListener('click', changeColor)
 function changeColor(el) {
+  const randomColor = getRandomHexColor();
   body.style.backgroundColor = randomColor;
   spanColor.textContent = randomColor;
 }
